3311bfd321137afd2f9fa2f00c0c41048d327eb1
galgantar <gantar.gal@gmail.com>
2020-04-28 23:06:14 +0200

    Dependencies -> vendor

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,6 @@
 workspace "powerpit"
 	architecture "x86_64"
-	
+
 	configurations
 	{
 		"Debug",
@@ -25,32 +25,32 @@ project "powerpit"
 
 		"%{prj.name}/shaders/*.shader",
 
-		"%{prj.name}/dependencies/glad/**.h",
-		"%{prj.name}/dependencies/glad/**.c",
+		"%{prj.name}/vendor/glad/**.h",
+		"%{prj.name}/vendor/glad/**.c",
 
-		"%{prj.name}/dependencies/glfw/include/GLFW/**.h",
-		"%{prj.name}/dependencies/glfw/src/glfw_config.h",
-		"%{prj.name}/dependencies/glfw/src/context.c",
-		"%{prj.name}/dependencies/glfw/src/init.c",
-		"%{prj.name}/dependencies/glfw/src/input.c",
-		"%{prj.name}/dependencies/glfw/src/monitor.c",
-		"%{prj.name}/dependencies/glfw/src/vulkan.c",
-		"%{prj.name}/dependencies/glfw/src/window.c",
-		"%{prj.name}/dependencies/glfw/src/win32_init.c",
-		"%{prj.name}/dependencies/glfw/src/win32_joystick.c",
-		"%{prj.name}/dependencies/glfw/src/win32_monitor.c",
-		"%{prj.name}/dependencies/glfw/src/win32_time.c",
-		"%{prj.name}/dependencies/glfw/src/win32_thread.c",
-		"%{prj.name}/dependencies/glfw/src/win32_window.c",
-		"%{prj.name}/dependencies/glfw/src/wgl_context.c",
-		"%{prj.name}/dependencies/glfw/src/egl_context.c",
-		"%{prj.name}/dependencies/glfw/src/osmesa_context.c",
+		"%{prj.name}/vendor/glfw/include/GLFW/**.h",
+		"%{prj.name}/vendor/glfw/src/glfw_config.h",
+		"%{prj.name}/vendor/glfw/src/context.c",
+		"%{prj.name}/vendor/glfw/src/init.c",
+		"%{prj.name}/vendor/glfw/src/input.c",
+		"%{prj.name}/vendor/glfw/src/monitor.c",
+		"%{prj.name}/vendor/glfw/src/vulkan.c",
+		"%{prj.name}/vendor/glfw/src/window.c",
+		"%{prj.name}/vendor/glfw/src/win32_init.c",
+		"%{prj.name}/vendor/glfw/src/win32_joystick.c",
+		"%{prj.name}/vendor/glfw/src/win32_monitor.c",
+		"%{prj.name}/vendor/glfw/src/win32_time.c",
+		"%{prj.name}/vendor/glfw/src/win32_thread.c",
+		"%{prj.name}/vendor/glfw/src/win32_window.c",
+		"%{prj.name}/vendor/glfw/src/wgl_context.c",
+		"%{prj.name}/vendor/glfw/src/egl_context.c",
+		"%{prj.name}/vendor/glfw/src/osmesa_context.c",
 
-		"%{prj.name}/dependencies/gmath/*.h",
-		"%{prj.name}/dependencies/gmath/*.hpp",
+		"%{prj.name}/vendor/gmath/*.h",
+		"%{prj.name}/vendor/gmath/*.hpp",
 
-		"%{prj.name}/dependencies/SOIL2/src/SOIL2/*.h",
-		"%{prj.name}/dependencies/SOIL2/src/SOIL2/*.c"
+		"%{prj.name}/vendor/SOIL2/src/SOIL2/*.h",
+		"%{prj.name}/vendor/SOIL2/src/SOIL2/*.c"
 	}
 
 	defines
@@ -59,15 +59,15 @@ project "powerpit"
 
 		-- for glfw
 		"_GLFW_WIN32",
-		"_CRT_SECURE_NO_WARNINGS" 
+		"_CRT_SECURE_NO_WARNINGS"
 	}
 
 	includedirs
 	{
-		"%{prj.name}/dependencies/glfw/include/GLFW",
-		"%{prj.name}/dependencies/glad/include",
-		"%{prj.name}/dependencies/gmath",
-		"%{prj.name}/dependencies/SOIL2/src/SOIL2",
+		"%{prj.name}/vendor/glfw/include/GLFW",
+		"%{prj.name}/vendor/glad/include",
+		"%{prj.name}/vendor/gmath",
+		"%{prj.name}/vendor/SOIL2/src/SOIL2",
 	}
 
 	links
